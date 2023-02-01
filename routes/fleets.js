d1d3a71d1a851131be58c053269278998a9c8edc
Laura Perote <laury0luz@example.com>
2023-02-01 23:15:01 +0100

    Added skeleton

---
+++ routes/fleets.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/', function(req, res, next) {
+  //TODO
+});
+
+router.delete('/', function(req, res, next) {
+  //TODO
+});
+  
+
+module.exports = router;
