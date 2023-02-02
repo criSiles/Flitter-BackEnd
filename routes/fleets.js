5297b9c514b151f7053abab9ce825bc00884ec4f
Laura Perote <laury0luz@example.com>
2023-02-02 19:02:06 +0100

    Added creation and deletion endpoints for fleets

--- routes/fleets.js
+++ routes/fleets.js
@@ -1,12 +1,28 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+const Fleet = require("../models/Fleet.js")
 
-router.post('/', function(req, res, next) {
-  //TODO
+router.get('/', async (req, res, next)=>{
+  res.send('TODO')
+})
+
+router.post('/', async (req, res, next)=>{
+  const fleetPost = new Fleet({
+    id_user: req.body.id_user,
+    text: req.body.text,
+    img: req.body.img,
+	});
+
+	await Fleet.create(fleetPost)
+	res.send(fleetPost)
 });
 
-router.delete('/', function(req, res, next) {
-  //TODO
+router.delete('/:id', async (req, res, next) => {
+  await Fleet.deleteOne({_id: req.params.id}).then(() => {
+      res.send();
+    }).catch(()=>{
+      res.status(404).send()
+    })
 });
   
 
