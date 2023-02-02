9d5f55ed7209667299b434d75f42002d7cf3bb4d
RocioRDev <rociorgdev@gmail.com>
2023-02-02 21:19:54 +0100

    create route for find user by id

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,11 @@ router.get('/list', function(req, res, next) {
   userController.userList(req, res, next);
 });
 
+/* GET user by id. */
+router.get('/:id', function(req, res, next) {
+  userController.userGetById(req, res, next);
+});
+
 /* POST create user. */
 router.post('/', function(req, res, next) {
   userController.userCreate(req, res, next);
