0841b753ffd3b015476bc7df15cb4b1e402c6084
LuciaSaenz <luciasaenz92@hotmail.com>
2023-02-04 11:19:59 +0100

    added CORS settings for front/back sync

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,6 +18,7 @@ app.locals.title = "Flitter";
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
