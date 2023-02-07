a3fea69d625468d1a6fc97f66ce2ed9964f8828a
RocioRDev <rociorgdev@gmail.com>
2023-02-07 19:29:29 +0100

    Basic login
    added kudos endpoints
    
    added encrypt to initial users to password1

--- app.js
+++ app.js
@@ -1,49 +1,70 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const session = require("express-session");
+const authChecker = require("./utils/authChecker");
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 var fleetsRouter = require("./routes/api/fleets");
 var kudosRouter = require("./routes/api/kudos");
 
-require('./lib/connectMongoose');
+require("./lib/connectMongoose");
 
 var app = express();
 
 app.locals.title = "Flitter";
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+const expireTime = 1000 * 60 * 60 * 2;
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: expireTime },
+  })
+);
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Configurar swagger
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./api-flitter/apidoc.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./api-flitter/apidoc.json");
+
+app.use(cors());
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
-
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', false);
+  res.setHeader("Access-Control-Allow-Credentials", false);
 
   // Pass to next layer of middleware
   next();
@@ -57,21 +78,20 @@ app.use('/api/fleets', fleetsRouter);
 app.use('/api/kudos', kudosRouter);
 app.use(express.static("public"))
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,104 +1,141 @@
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
-const path = require('path');
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const jwt = require("jsonwebtoken");
 
 // POST create new user (Restriction: you cannot create a user with an existing email address.)
 exports.userCreate = async (req, res) => {
-    try {
-        // Get the data from the request    
-        const { email, password, name, avatar } = req.body;
-        // Create a new user
-        const user = new User({ email, password, name, avatar });
-        // Check if the email is already registered
-        const emailExists = await User.findOne({ email });
-        if (emailExists) {
-            return res.status(400).json({ error: "The e-mail address is already registered" });
-        }
-        // Encrypt the password
-        const salt = await bcrypt.genSalt(10);
-        const hashPassword = await bcrypt.hash(password, salt);
-        user.password = hashPassword;
-        // Save the user
-        await user.save();
-        return res.status(201).json({ message: "User created!" });        
-    } catch (err) {
-        return res.status(500).json({ error: "Error creating user" });
-    }    
+  try {
+    // Get the data from the request
+    const { email, password, name, avatar } = req.body;
+    // Create a new user
+    const user = new User({ email, password, name, avatar });
+    // Check if the email is already registered
+    const emailExists = await User.findOne({ email });
+    if (emailExists) {
+      return res
+        .status(400)
+        .json({ error: "The e-mail address is already registered" });
+    }
+    // Encrypt the password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+    user.password = hashPassword;
+    // Save the user
+    await user.save();
+    return res.status(201).json({ message: "User created!" });
+  } catch (err) {
+    return res.status(500).json({ error: "Error creating user" });
+  }
 };
 
 // GET user list
 exports.userList = (req, res, next) => {
-    User.find({}, (err, users) => {
-        if (err) {
-            return next(err);
-        }
-        res.status(200).json({ users });
-    });
+  User.find({}, (err, users) => {
+    if (err) {
+      return next(err);
+    }
+    res.status(200).json({ users });
+  });
 };
 
 // GET user by id
-exports.userGetById = function(req, res, next) {
-    User.findById(req.params.id, (err, user) => {
-        if (err) {
-            return next(err);
-        }
-        res.status(200).json({ user });
-    });
+exports.userGetById = function (req, res, next) {
+  User.findById(req.params.id, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    res.status(200).json({ user });
+  });
 };
 
 // GET user by email
-exports.userGetByEmail = function(req, res, next) {
-    User.findOne({ email: req.params.email }, (err, user) => {
-        if (err) {
-            return next(err);
-        }
-        res.status(200).json({ user });
-    });
+exports.userGetByEmail = function (req, res, next) {
+  User.findOne({ email: req.params.email }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    res.status(200).json({ user });
+  });
 };
 
 // PUT update user by id
 exports.userUpdateById = async (req, res) => {
-    try {
-        const { email } = req.body; // Obtain the e-mail address of the request body
-    
-        // Check if the user exists
-        const user = await User.findById(req.params.id);
-        if (!user) {
-          return res.status(404).json({ error: "User not found" });
-        }
-    
-        // Check if the new email is already registered
-        if (email && email !== user.email) {
-          const emailExists = await User.findOne({ email });
-          if (emailExists) {
-            return res.status(400).json({ error: "The e-mail address is already registered" });
-          }
-        }
-    
-        // Update the user
-        Object.assign(user, req.body);
-        await user.save();
-    
-        return res.json({ message: "Updated user!" });
-      } catch (error) {
-        return res.status(500).json({ error: "Error updating user" });
+  try {
+    const { email } = req.body; // Obtain the e-mail address of the request body
+
+    // Check if the user exists
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Check if the new email is already registered
+    if (email && email !== user.email) {
+      const emailExists = await User.findOne({ email });
+      if (emailExists) {
+        return res
+          .status(400)
+          .json({ error: "The e-mail address is already registered" });
       }
+    }
+
+    // Update the user
+    Object.assign(user, req.body);
+    await user.save();
+
+    return res.json({ message: "Updated user!" });
+  } catch (error) {
+    return res.status(500).json({ error: "Error updating user" });
+  }
 };
 
 // DELETE user by id
 exports.userDeleteById = async (req, res) => {
-    try {
-        // Check if the user exists
-        const user = await User.findById(req.params.id);
-        if (!user) {
-          return res.status(404).json({ error: "User not found" });
-        }
-    
-        // Delete user
-        await user.remove();
-    
-        return res.json({ message: "User successfully deleted" });
-      } catch (error) {
-        return res.status(500).json({ error: "Error deleting user" });
-      }
-}
+  try {
+    // Check if the user exists
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Delete user
+    await user.remove();
+
+    return res.json({ message: "User successfully deleted" });
+  } catch (error) {
+    return res.status(500).json({ error: "Error deleting user" });
+  }
+};
+
+// GET logout user
+exports.userLogout = async (req, res) => {
+  console.log(req.session);
+  req.session.destroy();
+  res.status(200).json({ message: "Logged out!" });
+  // res.redirect('/');
+};
+
+// POST login user with body
+exports.userLogin = async (req, res) => {
+  // Get the data from the request
+  const { email, password } = req.body;
+  // Check if the user exists
+  User.findOne({ email: email }, async (err, user) => {
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    // Check if the password is correct
+    console.log(user);
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+    // Create and assign a token
+    const token = jwt.sign({ _id: user._id }, "TOKEN_SECRET", {
+      expiresIn: "1h",
+    });
+    //res.header('auth-token', token).json({ token });
+    return res.status(200).json({ message: "Logged in!", authToken: token });
+  });
+};
