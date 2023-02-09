c58221195088c9b5d53112bebf46a9c2d999eb00
Cris Siles <cristina.siles.vega@gmail.com>
2023-02-09 18:12:48 +0100

    Regex fixed

--- routes/api/fleets.js
+++ routes/api/fleets.js
@@ -35,7 +35,7 @@ router.get("/", async (req, res, next) => {
     }
 
     if (text) {
-      filter.text = new RegExp("^" + req.query.text, "i");
+      filter.text = new RegExp(req.query.text, "i");
     }
 
     if (kudos) {
